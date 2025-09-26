bc1e8984b3ddad184f1ed9554a2b170ef0a3af47
Kwon <korpgm@gmail.com>
2025-09-26 17:22:33 +0900

    feat: actuator 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    // actuator + prometheus
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
